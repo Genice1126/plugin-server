c971bc159570d4cb86845bc59c905b2ef8e3a74a
刘涛 <liutao.080427@gmail.com>
2018-12-04 09:42:48 +0800

    plugin-redis

--- config/index.js
+++ config/index.js
@@ -1,10 +1,9 @@
-const debug = require('debug')('plugin-config');
 process.env.NODE_ENV = (process.env.NODE_ENV) ? process.env.NODE_ENV : 'localhost';
-debug('插件运行环境=====>>>' , process.env.NODE_ENV);
+const debug = require('debug')('plugin-config');
 const path = require('path');
-const config_env = require(path.join(__dirname , process.env.NODE_ENV));
 const common_config = require('./common');
+debug('插件运行环境=====>>>' , process.env.NODE_ENV);
+const config_env = require(path.join(__dirname , process.env.NODE_ENV));
 const loading_config = Object.assign(common_config , config_env);
-
 exports = module.exports = loading_config;
 
--- config/localhost.js
+++ config/localhost.js
@@ -1,5 +1,13 @@
 
 //本地调试配置
 module.exports = {
-    b: 1
+    
+    redisServer : {
+        host : "localhost",
+        port : 6379,
+        return_buffers : false, //true 返回的数据为buffer，
+        socket_keepalive : true,  //在底层套接字上启用keep-alive功能
+        disable_resubscribing : false, //true 断开连接后客户端不会重新订阅
+    }
+
 }
