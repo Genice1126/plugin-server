b184861db86e23209931da73231f05a3f60e7f3c
刘涛 <liutao.080427@gmail.com>
2018-12-19 09:05:06 +0800

    save

--- modules/plugin-bunyan/index.js
+++ modules/plugin-bunyan/index.js
@@ -1,5 +1,7 @@
 
-
+/**
+ * 用起来简单 性能好
+ */
 
 /**
  * 日志系统(想要什么)
@@ -54,27 +56,40 @@
 //   foo: 'bar',
 // });
 
+/**
+ *  name可以自己定义
+ *  是否有日志记录 streams中是否有path
+ *  
+ */
 
-let config = {
-  info : {
-    is_open : "",
-    path : "",
-    level : ""
-  },
-  warn : {
+const path = require('path');
+const Bunyan = require('bunyan');
+const sys_config = require('../../config');
 
-  },
-  error : {
 
-  }
-}
+let log = Bunyan.createLogger({
+  name : "app"
+});
 
-function P (){
+log.info();     // Returns a boolean: is the "info" level enabled?
+                // This is equivalent to `log.isInfoEnabled()` or
+                // `log.isEnabledFor(INFO)` in log4j.
 
-}
+log.info('hi');                     // Log a simple string message (or number).
+log.info('hi %s', 1, 2); // Uses `util.format` for msg formatting.
 
-Object.keys().forEach( key => {
+log.info({foo: 'bar'}, 'hi');
+                // The first field can optionally be a "fields" object, which
+                // is merged into the log record.
 
-})
+log.info('err');  // Special case to log an `Error` instance to the record.
+                // This adds an "err" field with exception details
+                // (including the stack) and sets "msg" to the exception
+                // message.
+log.info('err', 'more on this: %s', 123);
+                // ... or you can specify the "msg".
+
+log.info({foo: 'bar', err: 'err'}, 'some msg about this error');
+
 
 
--- modules/plugin-bunyan/product-log.js
+++ modules/plugin-bunyan/product-log.js
@@ -10,27 +10,73 @@ const sys_config = require('../../config');
 
 //new LogProduct({name : "app"});
 
-
-class P {
-
-  constructor (config){
-
-    this.config = Object.assign({
-      name : "default",
-    },config)
-    this.path = path.join(sys_config.log_path , this.config.name , );
-
+let config = {
+  info : {
+    level : 'info',
+    path : ""
+  },
+  error : {
+    level : 'error',
+    path : ""
   }
+}
 
-  _create_dir (dirname){
-    if(fs.existsSync(dirname)){
-      return true
-    }else if(this._create_dir(path.dirname(dirname))){
-      fs.mkdirSync(dirname);
-      return true
+
+function ProducerLog (custom_config){
+  this.custom_config = Object.assign({
+    name : "default"
+  },custom_config)
+}
+
+function _create_dir(dirname){
+  if(fs.existsSync(dirname)){
+    return true;
+  }else if(_create_dir(path.dirname(dirname))){
+    fs.mkdirSync(dirname);
+    return true;
+  }
+}
+
+function log_constructor_fn(key , val){
+  console.log('key==val' , key , val);
+  let log_path = (val.path) ? path.join(val.path , key + '.log') : "";
+  let log_level = (val.level) ? val.level : key;
+
+  return function (...param){
+    console.log(...param);
+  
+    if(log_path){
+      this.custom_config.streams = [{path : log_path}]
+    }else{
+      this.custom_config.stream = process.stdout;
     }
+    console.log('length====>>' , param.length);
+    if(param.length > 1){
+      this.custom_config.serializers = {
+        [_init_serializer] : _init_serializer
+      }
+    }
+    console.log('this.custom_config===>' , this.custom_config);
+    if(!this.bunyan_client) this.bunyan_client = Bunyan.createLogger(this.custom_config);
+    let param_pop = param.pop();
+    param_pop = (param_pop.__proto__ === String) ? param_pop : JSON.stringify(param_pop);
+    this.bunyan_client[key].call(this.bunyan_client , ...param , param_pop);
+    
   }
-
-
 
 }
+
+function _init_serializer (field){
+  return field;
+}
+
+function _init_serializer_val (val){
+  return val;
+}
+
+Object.keys(config).forEach(key => {
+  ProducerLog.prototype[key] = log_constructor_fn(key , config[key]);
+})
+
+let a = new ProducerLog();
+a.info({aaa : 1},{tye : 123});
